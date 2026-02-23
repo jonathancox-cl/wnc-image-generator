663f0196cf5a4982230507d993a66e67b9a73e7e
jonathancox-cl <jonathancox.business@gmail.com>
2026-02-23 11:47:12 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 apt-get install -y fonts-dejavu fonts-crosextra-caladea || true
 pip install -r requirements.txt
+#!/bin/bash
+apt-get update && apt-get install -y \
+  libjpeg-dev \
+    zlib1g-dev \
+      libpng-dev \
+        fonts-dejavu \
+          fonts-crosextra-caladea \
+            || true
+            pip install --upgrade pip
+            pip install -r requirements.txt
